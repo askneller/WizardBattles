3598b9ae6ef919982335ff367dacd3a809ffda5e
Adam Kneller <a_kneller@yahoo.com.au>
2019-09-02 19:36:05 +1000

    Initial commit, scroll casts fireball, generates mana ore and can crush ore

---
+++ src/main/java/org/terasology/world/ForagableFoodRasterizer.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.world;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
+import org.terasology.math.geom.BaseVector3i;
+import org.terasology.registry.CoreRegistry;
+import org.terasology.utilities.procedural.WhiteNoise;
+import org.terasology.world.block.Block;
+import org.terasology.world.block.BlockManager;
+import org.terasology.world.chunks.CoreChunk;
+import org.terasology.world.generation.Region;
+import org.terasology.world.generation.WorldRasterizerPlugin;
+import org.terasology.world.generator.plugin.RegisterPlugin;
+
+import java.util.List;
+import java.util.Map;
+
+@RegisterPlugin
+public class ForagableFoodRasterizer implements WorldRasterizerPlugin {
+    private final Map<ForagableFoodType, List<Block>> foragableFood = Maps.newEnumMap(ForagableFoodType.class);
+
+    private BlockManager blockManager;
+
+    @Override
+    public void initialize() {
+        blockManager = CoreRegistry.get(BlockManager.class);
+
+//        foragableFood.put(ForagableFoodType.JOSHABERRY, ImmutableList.<Block>of(
+//                blockManager.getBlock("JoshariasSurvival:JoshaberryBushFull")));
+        foragableFood.put(ForagableFoodType.ROCK, ImmutableList.<Block>of(
+                blockManager.getBlockFamily("WizardTowers:Rock").getArchetypeBlock()));
+    }
+
+    @Override
+    public void generateChunk(CoreChunk chunk, Region chunkRegion) {
+        ForagableFoodFacet facet = chunkRegion.getFacet(ForagableFoodFacet.class);
+        Block air = blockManager.getBlock(BlockManager.AIR_ID);
+
+        WhiteNoise noise = new WhiteNoise(chunk.getPosition().hashCode());
+
+        Map<BaseVector3i, ForagableFoodType> entries = facet.getRelativeEntries();
+        for (BaseVector3i pos : entries.keySet()) {
+
+            // check if some other rasterizer has already placed something here
+            if (chunk.getBlock(pos).equals(air)) {
+
+                ForagableFoodType type = entries.get(pos);
+                List<Block> list = foragableFood.get(type);
+                int blockIdx = Math.abs(noise.intNoise(pos.getX(), pos.getY(), pos.getZ())) % list.size();
+                Block block = list.get(blockIdx);
+                chunk.setBlock(pos, block);
+            }
+        }
+    }
+}
