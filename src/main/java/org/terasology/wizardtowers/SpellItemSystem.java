af31fa4df97758c8f5538b11fe83aa8acc71e9a7
Adam Kneller <a_kneller@yahoo.com.au>
2019-11-20 19:22:35 +1000

    Implement SpellItem charges

---
+++ src/main/java/org/terasology/wizardtowers/SpellItemSystem.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2019 MovingBlocks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.terasology.wizardtowers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.terasology.combatSystem.weaponFeatures.components.LaunchEntityComponent;
+import org.terasology.combatSystem.weaponFeatures.events.LaunchEntityEvent;
+import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.event.ReceiveEvent;
+import org.terasology.entitySystem.systems.BaseComponentSystem;
+import org.terasology.entitySystem.systems.RegisterSystem;
+import org.terasology.logic.common.ActivateEvent;
+
+@RegisterSystem
+public class SpellItemSystem extends BaseComponentSystem {
+
+    private static final Logger logger = LoggerFactory.getLogger(SpellItemSystem.class);
+
+    @ReceiveEvent(components = SpellItemComponent.class)
+    public void cast(ActivateEvent event, EntityRef entity) {
+        SpellItemComponent spellItemComponent = entity.getComponent(SpellItemComponent.class);
+        if (spellItemComponent.charges > 0) {
+            spellItemComponent.charges--;
+            entity.saveComponent(spellItemComponent);
+            entity.send(new CastEvent(event));
+        } else if (spellItemComponent.charges == -1) {
+            // Unlimited charges
+            entity.send(new CastEvent(event));
+        }
+    }
+
+    @ReceiveEvent(components = LaunchEntityComponent.class)
+    public void onFire(CastEvent event, EntityRef entity) {
+        entity.send(new LaunchEntityEvent(event.getDirection()));
+    }
+
+}
