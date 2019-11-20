e251fccd960800bb79b0009606b86927d1c83498
Adam Kneller <a_kneller@yahoo.com.au>
2019-11-20 19:07:46 +1000

    Add CombatSystem as dependency, use that for launching from wand

--- src/main/java/org/terasology/wizardtowers/CastingSystem.java
+++ src/main/java/org/terasology/wizardtowers/CastingSystem.java
@@ -15,9 +15,11 @@
  */
 package org.terasology.wizardtowers;
 
-//import org.terasology.combatSystem.weaponFeatures.OwnerSpecific;
-//import org.terasology.combatSystem.weaponFeatures.components.LaunchEntityComponent;
-//import org.terasology.combatSystem.weaponFeatures.events.LaunchEntityEvent;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.terasology.combatSystem.weaponFeatures.OwnerSpecific;
+import org.terasology.combatSystem.weaponFeatures.components.LaunchEntityComponent;
+import org.terasology.combatSystem.weaponFeatures.events.LaunchEntityEvent;
 import org.terasology.entitySystem.entity.EntityRef;
 import org.terasology.entitySystem.event.ReceiveEvent;
 import org.terasology.entitySystem.systems.BaseComponentSystem;
@@ -28,13 +30,16 @@ import org.terasology.logic.inventory.ItemComponent;
 @RegisterSystem
 public class CastingSystem extends BaseComponentSystem {
 
-    @ReceiveEvent(components = SpellComponent.class)
+    private static final Logger logger = LoggerFactory.getLogger(CastingSystem.class);
+
+    @ReceiveEvent(components = SpellItemComponent.class)
     public void cast(ActivateEvent event, EntityRef entity) {
+//        logger.info("Spell item activated");
         // Find the caster
         EntityRef caster = EntityRef.NULL;
 
         if (entity.hasComponent(ItemComponent.class)) {
-//            caster = OwnerSpecific.getUltimateOwner(entity);
+            caster = OwnerSpecific.getUltimateOwner(entity);
         }
 
         // if no owner of "entity" is present then "entity" becomes "caster". e.g. world generated
@@ -46,25 +51,31 @@ public class CastingSystem extends BaseComponentSystem {
         }
 
         // Reduce the caster's mana by the cost of the spell
-        ManaComponent casterMana = caster.getComponent(ManaComponent.class);
-        if (casterMana != null) {
-            // Was cast by a player or NPC, reduce their Mana
-            SpellComponent spellComponent = entity.getComponent(SpellComponent.class);
-            int current = casterMana.current;
-            if (current >= spellComponent.manaCost) {
-                casterMana.current -= spellComponent.manaCost;
-                caster.saveComponent(casterMana);
-                // Complete the spell cast
-                entity.send(new CastEvent(event));
-            }
-        } // else was cast by trap or something else
+//        ManaComponent casterMana = caster.getComponent(ManaComponent.class);
+//        if (casterMana != null) {
+//            // Was cast by a player or NPC, reduce their Mana
+//            SpellComponent spellComponent = entity.getComponent(SpellComponent.class);
+//            int current = casterMana.current;
+//            if (current >= spellComponent.manaCost) {
+//                casterMana.current -= spellComponent.manaCost;
+//                caster.saveComponent(casterMana);
+//                // Complete the spell cast
+//                entity.send(new CastEvent(event));
+//            }
+//        } // else was cast by trap or something else
+        SpellItemComponent spellItemComponent = entity.getComponent(SpellItemComponent.class);
+        if (spellItemComponent.charges > 0 || spellItemComponent.charges == -1) {
+//            logger.info("Sending cast event");
+            entity.send(new CastEvent(event));
+        }
         // TODO handle the case where a non-wizard entity (e.g. NPC) tries to use
         //  something like a spell scroll, maybe create a CasterComponent
     }
 
-//    @ReceiveEvent(components = LaunchEntityComponent.class)
-//    public void onFire(CastEvent event, EntityRef entity) {
-//        entity.send(new LaunchEntityEvent(event.getDirection()));
-//    }
+    @ReceiveEvent(components = LaunchEntityComponent.class)
+    public void onFire(CastEvent event, EntityRef entity) {
+//        logger.info("CastEvent received");
+        entity.send(new LaunchEntityEvent(event.getDirection()));
+    }
 
 }
