39e22b84737f53e19a3b1cec083033805a4c2e24
Adam Kneller <a_kneller@yahoo.com.au>
2020-01-07 19:16:53 +1000

    Update ManaCrystal configuration

--- src/main/java/org/terasology/wizardtowers/ManaCrystalCaveProvider.java
+++ src/main/java/org/terasology/wizardtowers/ManaCrystalCaveProvider.java
@@ -21,21 +21,26 @@ import org.terasology.caves.CaveFloorFacet;
 import org.terasology.entitySystem.Component;
 import org.terasology.math.Region3i;
 import org.terasology.math.TeraMath;
-import org.terasology.math.geom.Vector3i;
 import org.terasology.rendering.nui.properties.Range;
 import org.terasology.utilities.procedural.Noise;
 import org.terasology.utilities.procedural.WhiteNoise;
-import org.terasology.world.generation.*;
+import org.terasology.world.generation.ConfigurableFacetProvider;
+import org.terasology.world.generation.Facet;
+import org.terasology.world.generation.FacetProviderPlugin;
+import org.terasology.world.generation.GeneratingRegion;
+import org.terasology.world.generation.Produces;
+import org.terasology.world.generation.Requires;
+import org.terasology.world.generation.facets.SurfaceHeightFacet;
 import org.terasology.world.generator.plugin.RegisterPlugin;
 
 
 @RegisterPlugin
 @Produces(ManaCrystalFacet.class)
-@Requires(@Facet(CaveFloorFacet.class))
+@Requires({@Facet(CaveFloorFacet.class), @Facet(value = SurfaceHeightFacet.class)})
 public class ManaCrystalCaveProvider implements ConfigurableFacetProvider, FacetProviderPlugin {
+
     private static final Logger logger = LoggerFactory.getLogger(ManaCrystalCaveProvider.class);
     private Noise densityNoiseGen;
-    private boolean hasPrinted = false;
 
     private ManaCrystalDensityConfiguration configuration = new ManaCrystalDensityConfiguration();
 
@@ -54,28 +59,29 @@ public class ManaCrystalCaveProvider implements ConfigurableFacetProvider, Facet
         int minY = worldRegion.minY();
         int maxY = worldRegion.maxY();
 
-        Vector3i pos = new Vector3i();
-
         for (int z = worldRegion.minZ(); z <= worldRegion.maxZ(); z++) {
             for (int x = worldRegion.minX(); x <= worldRegion.maxX(); x++) {
-                float h = floorFacet.getWorld(x, z);
-                int height = TeraMath.floorToInt(h);
+                float caveFloorHeight = floorFacet.getWorld(x, z);
+                int caveFloorInt = TeraMath.floorToInt(caveFloorHeight);
 
                 // if the surface is in range
-                if (height >= minY && height <= maxY) {
-
-                    pos.set(x, height, z);
-
-                    if (Math.abs(densityNoiseGen.noise(x, z)) < configuration.density) {
-                        facet.setWorld(x, height, z, ManaCrystalType.DEFAULT);
-//                        if (x >= 0 && x < 32 && z >= 0 && z < 32) {
-//                            logger.info("Placed crystal at {} {} {}", x, height, z);
-//                        }
+                if (hasCave(caveFloorHeight) && caveFloorInt >= minY && caveFloorInt <= maxY) {
+                    // Does it meet depth requirements
+                    SurfaceHeightFacet surfaceHeightFacet = region.getRegionFacet(SurfaceHeightFacet.class);
+                    float s = surfaceHeightFacet.getWorld(x, z);
+                    int surface = TeraMath.floorToInt(s);
+                    boolean isDeepEnough = caveFloorInt < (float) surface - configuration.minDepth;
+                    if (isDeepEnough && Math.abs(densityNoiseGen.noise(x, z)) < configuration.density) {
+                        facet.setWorld(x, caveFloorInt + 1, z, ManaCrystalType.DEFAULT);
                     }
                 }
             }
         }
         region.setRegionFacet(ManaCrystalFacet.class, facet);
+    }
+
+    private boolean hasCave(float caveFloor) {
+        return !Float.isNaN(caveFloor);
     }
 
     @Override
@@ -97,6 +103,8 @@ public class ManaCrystalCaveProvider implements ConfigurableFacetProvider, Facet
         @Range(min = 0, max = 1.0f, increment = 0.01f, precision = 2, description = "Define the overall amount of mana crystals")
         private float density = 0.01f;
 
+        @Range(min = 0, max = 250f, increment = 1f, precision = 0, description = "The minimum distance below the surface")
+        private float minDepth = 30f;
     }
 
 }
