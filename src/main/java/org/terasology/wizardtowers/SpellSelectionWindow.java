79f271ddf1bff72b1e09a3621fc2c141e927776c
Adam Kneller <a_kneller@yahoo.com.au>
2020-01-13 20:45:19 +1000

    Add spell selection system and ui

--- src/main/java/org/terasology/wizardtowers/SpellSelectionWindow.java
+++ src/main/java/org/terasology/wizardtowers/SpellSelectionWindow.java
@@ -16,24 +16,29 @@
 package org.terasology.wizardtowers;
 
 import org.terasology.entitySystem.entity.EntityRef;
+import org.terasology.entitySystem.prefab.Prefab;
+import org.terasology.logic.common.DisplayNameComponent;
 import org.terasology.logic.players.LocalPlayer;
 import org.terasology.registry.CoreRegistry;
 import org.terasology.rendering.nui.databinding.Binding;
 import org.terasology.rendering.nui.databinding.ReadOnlyBinding;
 import org.terasology.rendering.nui.layers.hud.CoreHudWidget;
 import org.terasology.rendering.nui.widgets.UIText;
+import org.terasology.utilities.Assets;
+
+import java.util.Optional;
 
 
-public class ManaWindow extends CoreHudWidget {
+public class SpellSelectionWindow extends CoreHudWidget {
 
     @Override
     public void initialise() {
-        UIText uiText = find("mana", UIText.class);
+        UIText uiText = find("spellSelection", UIText.class);
         uiText.bindVisible(new ReadOnlyBinding<Boolean>() {
             @Override
             public Boolean get() {
                 EntityRef character = CoreRegistry.get(LocalPlayer.class).getCharacterEntity();
-                return character != null && character.hasComponent(ManaComponent.class);
+                return character != null && character.hasComponent(GrimoireComponent.class);
             }
         });
         uiText.bindText(
@@ -41,12 +46,25 @@ public class ManaWindow extends CoreHudWidget {
                     @Override
                     public String get() {
                         EntityRef character = CoreRegistry.get(LocalPlayer.class).getCharacterEntity();
-                        if (character == null || !character.hasComponent(ManaComponent.class)) {
+                        if (character == null || !character.hasComponent(GrimoireComponent.class)) {
                             return "";
                         }
 
-                        ManaComponent mana = character.getComponent(ManaComponent.class);
-                        return mana.current + " / " + mana.maximum;
+                        SpellSelectionComponent spellSelectionComponent =
+                                character.getComponent(SpellSelectionComponent.class);
+                        String displayName = "None";
+                        if (spellSelectionComponent != null && spellSelectionComponent.selected != null
+                                && spellSelectionComponent.selected.length() > 0) {
+                            Optional<Prefab> optionalPrefab = Assets.getPrefab(spellSelectionComponent.selected);
+                            if (optionalPrefab.isPresent()) {
+                                Prefab prefab = optionalPrefab.get();
+                                DisplayNameComponent displayNameComponent = prefab.getComponent(DisplayNameComponent.class);
+                                if (displayNameComponent != null) {
+                                    displayName = displayNameComponent.name;
+                                }
+                            }
+                        }
+                        return displayName;
                     }
 
                     @Override
